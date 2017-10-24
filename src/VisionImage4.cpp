cff8d4a25193b89d77e870146f9c8d06670d07e7
Jannis Konopka <jakon17@student.sdu.dk>
2017-10-24 10:19:46 +0200

    little changes

--- src/VisionImage4.cpp
+++ src/VisionImage4.cpp
@@ -35,6 +35,7 @@ int main(int argc, char* argv[])
 
     //------------------------------Checked preconditions-----------------------
     //------------------------------Main Program starts-------------------------
+    cout << "Hello" << endl;
 
     imshow("Original", img);
     waitKey();
