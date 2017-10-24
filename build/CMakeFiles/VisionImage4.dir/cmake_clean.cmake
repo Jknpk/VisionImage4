44dbd94a07610c4080ab27ba72297746772eb33b
Jannis Konopka <jakon17@student.sdu.dk>
2017-10-24 10:07:00 +0200

    Init

---
+++ build/CMakeFiles/VisionImage4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VisionImage4.dir/src/VisionImage4.cpp.o"
+  "VisionImage4.pdb"
+  "VisionImage4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VisionImage4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/VisionImage4.cpp
@@ -0,0 +1,42 @@
+/* date:        24-10-2017
+ * author:      Jannis Konopka
+ * description:
+ */
+
+
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[])
+{
+    // Parse command line arguments
+    CommandLineParser parser(argc, argv,
+        "{help   |             | print this message}"
+        "{@image | ../lena.dib | image path}"
+    );
+
+    if (parser.has("help")) {
+        parser.printMessage();
+        return 0;
+    }
+
+    // Load image
+    string filename = parser.get<string>("@image");
+    Mat img = imread(filename); // ,IMREAD_GRAYSCALE (Load Picture as grayscale)
+
+    if (img.empty()) {
+        cout << "Input image not found at '" << filename << "'\n";
+        return 1;
+    }
+
+    //------------------------------Checked preconditions-----------------------
+    //------------------------------Main Program starts-------------------------
+
+    imshow("Original", img);
+    waitKey();
+    return 0;
+}
